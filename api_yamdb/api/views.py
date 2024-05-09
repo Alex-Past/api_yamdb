2b95d52b978559c236589e6d68c6c55d000c2fac
Фёдор Турлин <fedorturlin@gmail.com>
2024-05-09 23:11:24 +0300

    model, serializer, viewsSet, url for comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,10 +2,10 @@ from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import LimitOffsetPagination
 from django.db.models import Avg
+from django.shortcuts import get_object_or_404
 
-from api.serializers import TitleSerializer, CategorySerializer, \
-    GenreSerializer
-from reviews.models import Title, Category, Genre
+from api.serializers import TitleSerializer, CategorySerializer, CommentSerializer, ReviewSerializer, GenreSerializer
+from reviews.models import Title, Category, Genre, Review
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -42,3 +42,37 @@ class GenreViewSet(viewsets.ModelViewSet):
     # Жду прав доступа
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     filterset_fields = ('name_genre',)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = #Пермишшены моделей автора, модератора или админа
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = #пермишн модели автора, модератора или админа
+
+    def get_queryset(self):
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
