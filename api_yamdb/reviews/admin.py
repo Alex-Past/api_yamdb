7bf5b78e6985ec9193205bbd1b4e4cff919dfbed
Vasilina Alexeeva <alekseevasilina@yandex.ru>
2024-05-14 01:23:18 +0600

    Добавление реадми

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from reviews.models import Category, Genre, Title, Review, Comment
 
+
+admin.site.empty_value_display = 'Не задано'
+
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
