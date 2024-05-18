861dbd64cbed6e8aadaa3bddfb7193310ecd692f
Фёдор Турлин <fedorturlin@gmail.com>
2024-05-18 19:46:00 +0300

    serializer comment убрал лишнее поле вывода review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -77,10 +77,6 @@ class TitleWriteSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    review = serializers.SlugRelatedField(
-        slug_field='text',
-        read_only=True
-    )
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
