24daa01d17ced0fd478e65710130a0701d18656d
Фёдор Турлин <fedorturlin@gmail.com>
2024-05-18 19:51:51 +0300

    serializer review удалена лишняя валидация, вынес условие, изменил фильтрацию по id

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -93,21 +93,16 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
-    def validate_score(self, value):
-        if 0 > value > 10:
-            raise serializers.ValidationError('Оценка по 10-бальной шкале.')
-        return value
-
     def validate(self, data):
-        request = self.context['request']
-        author = request.user
+        """Запрещает пользователям оставлять повторные отзывы."""
+        if not self.context.get('request').method == 'POST':
+            return data
+        author = self.context.get('request').user
         title_id = self.context.get('view').kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        if (
-            request.method == 'POST'
-            and Review.objects.filter(title=title, author=author).exists()
-        ):
-            raise ValidationError('Может существовать только один отзыв!')
+        if Review.objects.filter(author=author, title=title_id).exists():
+            raise serializers.ValidationError(
+                'Вы уже оставляли отзыв на это произведение'
+            )
         return data
 
     class Meta:
